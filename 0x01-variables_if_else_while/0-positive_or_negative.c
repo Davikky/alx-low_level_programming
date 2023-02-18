12a20c9b2bcd6d11c478516d8c51480d5e31a67a
Davikky <ayobamivikky@gmail.com>
2023-02-17 18:55:50 -0800

    touching

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,10 @@
-if (n>0) {
+	if (n>0) {
 	printf("%d is positive.\n",n);
-}
-else {
-	if (n<0) {
-		printf("%d is negative.\n",n);
-	} else {
-		printf("%d is zero.\n",n);
 	}
-}
+	else {
+		if (n<0) {
+		printf("%d is negative.\n",n);
+		} else {
+		printf("%d is zero.\n",n);
+		}
+	}	
