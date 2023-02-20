4247a43ccdde3d1cb8782992b07306c98ba3f83a
Davikky <ayobamivikky@gmail.com>
2023-02-20 14:29:31 -0800

    Nested loop, What a feat!

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+/**
+* main - function in the main
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 100; a++)
+		{
+		int t1 = a / 10;
+		int u1 = a % 10;
+
+		for (b = a; b < 100; b++)
+			{
+			int t2 = b / 10;
+			int u2 = b % 10;
+
+			if (u1 != u2 && u1 < u2)
+				{
+				putchar(t1 + '0');
+				putchar(u1 + '0');
+				putchar(' ');
+				putchar(t2 + '0');
+				putchar(u2 + '0');
+
+				if (a < 99)
+					{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
