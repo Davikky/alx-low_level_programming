7a291bc441c10fa1b0fd70c7a73b8fb55a085184
Davikky <ayobamivikky@gmail.com>
2023-02-25 10:42:38 -0800

    Starting at A with 1 and at a with 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isupper - checks if argument ia uppercase
+ *
+ * @c: input parameter
+ * Return: Always 0
+ */
+
+int _isupper(int c)
+{
+	char upp = c;
+
+	if (upp >= 'A')
+	{
+		if (upp <= 'Z')
+		{
+			return (1);
+		}
+	}
+	else if (upp >= 'a')
+	{
+		if (upp <= 'z')
+		{
+			return (0);
+		}
+	}
+	return (0);
+}
