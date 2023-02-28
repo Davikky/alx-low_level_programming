12006911da5fdeb635f033be433fa1a6d7896af1
Davikky <ayobamivikky@gmail.com>
2023-02-28 07:50:42 -0800

    I could have left this half done...but for grit, I persevered, and won!

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * puts_half - prints second half of a string
+ *
+ * @str: String provided
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int i = 0;
+
+	int j;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	if ((i % 2) == 0)
+	{
+		for (j = (i / 2); j < i; j++)
+		{
+			_putchar(str[j]);
+		}
+	}
+	else
+	{
+		for (j = (i - 1) / 2; j < i; j++)
+		{
+			_putchar(str[j]);
+		}
+	}
+	_putchar('\n');
+}
