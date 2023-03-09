5f1b371460864055d9fde5328a98eb7c669e9feb
Davikky <ayobamivikky@gmail.com>
2023-03-09 14:24:52 -0800

    Now on C-Recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,11 +9,11 @@
 
 void _puts_recursion(char *s)
 {
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
+	if (*s)
 	{
-		_putchar(s[i]);
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
+	else
 	_putchar('\n');
 }
