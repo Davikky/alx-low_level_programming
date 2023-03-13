9db92edccd1b4af5947bc11236c7c8d5674a4b15
Davikky <ayobamivikky@gmail.com>
2023-03-13 15:43:18 -0700

    argc and argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: count of array stings
+ * @argv: array of pointers to strings; a Vector
+ *
+ * Return: Success
+ */
+
+int main(int argc, __attribute__ ((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
