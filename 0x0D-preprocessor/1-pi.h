2c4c9fc6105a708588fcd14ae7099759c24b3248
Davikky <ayobamivikky@gmail.com>
2023-03-21 04:02:45 -0700

    a header file that defines a macro named PI as an abbreviation for the token 3.14159265359

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,13 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/**
+ * Macro - A header file
+ *
+ * Desc: This defines a macro named PI which takes
+ *	the token 3.14159265359.
+ */
+
+#define PI 3.14159265359
+
+#endif /* OBJECT_LIKE_MACRO_H */
