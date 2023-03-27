21b12c7ca5120e8b1733f22678b2fda584861a99
Davikky <ayobamivikky@gmail.com>
2023-03-26 18:27:13 -0700

    a new type dog_t defined for the type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,4 +18,14 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+/**
+ * dog_t - typedef for stuct dog
+ */
+typedef struct dog dog_t;
+
+
+
+
+
 #endif /* DOG_H */
