11fa17401114385ff9ec2b721dd02d87f7871ca7
Davikky <ayobamivikky@gmail.com>
2023-03-26 17:25:28 -0700

    a structure type named dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+
+/**
+ * struct dog - a new type struct dog
+ * @name: first element
+ * @age: second element
+ * @owner: third element
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
