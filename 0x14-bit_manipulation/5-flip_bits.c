e0f5910c1d293ab8b38815cbdd7080183d1f18ad
Davikky <ayobamivikky@gmail.com>
2023-04-26 17:46:02 -0700

    how many bits to flip

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -15,14 +15,15 @@
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int compare, count = 0;
+	unsigned long int mask = 0, count = 0;
 
-	compare = n ^ m;
+	mask = n ^ m;
 
-	while (compare != 0)
+	while (mask)
 	{
-		count += (compare & 1);
-		compare >>= 1;
+		count += (mask & 1);
+
+		mask >>= 1;
 	}
 	return (count);
 }
