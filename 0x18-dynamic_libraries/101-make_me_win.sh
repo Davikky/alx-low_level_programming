d413e547e0460b81886320fde60579a6261365b0
Davikky <ayobamivikky@gmail.com>
2023-08-25 03:34:42 -0700

    3. Code injection: Win the Giga Millions!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P ~/alx-low_level_programming/0x18-dynamic_libraries/tests https://github.com/alx-tools/0x18.c/raw/master/gm
-export LD_PRELOAD=~/alx-low_level_programming/0x18-dynamic_libraries/tests/gm
+wget -P ./ https://github.com/alx-tools/0x18.c/raw/master/gm
+export LD_PRELOAD=./gm
