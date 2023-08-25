732f28d755217de7da00bfd5d47707eb2be681c7
Davikky <ayobamivikky@gmail.com>
2023-08-25 03:55:57 -0700

    for code injection

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/alx-tools/0x18.c/raw/master/gm
-export LD_PRELOAD=/tmp/gm
+wget -P /tmp github.com/Davikky/alx-low_level_programming/raw/master/0x18-dynamic_libraries/putshack.so
+export LD_PRELOAD=/tmp/putshack.so
